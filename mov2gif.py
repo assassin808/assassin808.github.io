c1814446bca05d132bd48f558e9c6133beed532d
assassin808 <93385065+assassin808@users.noreply.github.com>
2023-07-24 20:21:37 +0800

    Update mov2gif.py

--- mov2gif.py
+++ mov2gif.py
@@ -1,4 +1,4 @@
 from moviepy.editor import VideoFileClip
 
 videoClip = VideoFileClip("Untitled.mov")
-videoClip.write_gif("tankwar.gif")
+videoClip.write_gif("tankwar.gif", program='ffmpeg', fps=videoClip.fps/12)
